981cc7436d9374abe96932fbfe11dde3e5824cb6
Arphik <sebo44@gmail.com>
2020-07-22 00:15:19 +0200

    Fixed arrows in pagination. Now center in vertical.

--- src/js/sorting.test.js
+++ src/js/sorting.test.js
@@ -1,59 +1,48 @@
-import CompaniesData from './CompaniesData.js';
+import {sortResults} from './sort';
 
-
-sortingTest('Testing sort function', () => {
-});
-
-jest.mock('./js/CompaniesData'); // CompaniesData is now a mock constructor
-
-beforeEach(() => {
-  // Clear all instances and calls to constructor and all methods:
-  CompaniesData.mockClear();
-});
-
-it('We can check if the consumer called the class constructor', () => {
-  const companiesData = new CompaniesData();
-  expect(companiesData.sortResults('id', true, [
-      {
-          id: 4,
-          city: 'Wyszkow'
-      },
-      {
-          id: 5,
-          city: 'Wasawa'
-      },
-      {
-          id: 2,
-          city: 'Musawa'
-      },
-      {
-          id: 1,
-          city: 'Desawa'
-      },
-      {
-          id: 3,
-          city: 'Lasawa'
-      }
-  ]), toBe(Array([
-      {
-          id: 1,
-          city: 'Desawa'
-      },
-      {
-          id: 2,
-          city: 'Musawa'
-      },
-      {
-          id: 3,
-          city: 'Lasawa'
-      },
-      {
-          id: 4,
-          city: 'Wyszkow'
-      },
-      {
-          id: 5,
-          city: 'Wasawa'
-      }
-  ])));
+  
+test('Testing sorting objects by one property.',  () => {
+    expect(sortResults('id', true, [
+        {
+            id: 4,
+            city: 'Wyszkow'
+        },
+        {
+            id: 5,
+            city: 'Wasawa'
+        },
+        {
+            id: 2,
+            city: 'Musawa'
+        },
+        {
+            id: 1,
+            city: 'Desawa'
+        },
+        {
+            id: 3,
+            city: 'Lasawa'
+        }
+    ])).toBe(Array([
+        {
+            id: 1,
+            city: 'Desawa'
+        },
+        {
+            id: 2,
+            city: 'Musawa'
+        },
+        {
+            id: 3,
+            city: 'Lasawa'
+        },
+        {
+            id: 4,
+            city: 'Wyszkow'
+        },
+        {
+            id: 5,
+            city: 'Wasawa'
+        }
+    ]))
 });
